670736306f40032a13b3041f2401c0277dde1eac
criszapanta@gmail.com <criszapanta@gmail.com>
2022-02-22 18:25:53 +0800

    Initial commits-1

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,17 +1,7 @@
 async function createAuction(event, context) {
-    const { title } = JSON.parse(event.body);
-
-    const auction = {
-        title,
-        status: 'OPEN',
-        createdAt: NetworkInformation.toISOString(),
-    };
-
     return {
-        statusCode: 201,
-        body: JSON.stringify({auction}),
-
+        statusCode: 200,
+        body: JSON.stringify({event, context}),
     };
 }
-
 export const handler = createAuction;
